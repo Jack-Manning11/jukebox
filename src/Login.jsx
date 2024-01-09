567844a3d479b669b3e829b9e2ff7a9b51b8f018
Jack-Manning11 <jama0209@colorado.edu>
2024-01-09 08:56:31 -0700

    images update

--- src/Login.jsx
+++ src/Login.jsx
@@ -21,6 +21,7 @@ const Login = () => {
     ];
     const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=${scopes.join(" ")}`;
 
+
     return (
         <LoginContainer>
             <LoginInfo>
