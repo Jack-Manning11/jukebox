d4fc30f1d8009254ce0ae149ec8c725726fa2aee
Jack-Manning11 <jama0209@colorado.edu>
2024-04-17 10:48:45 -0600

    fix

--- src/Details.jsx
+++ src/Details.jsx
@@ -14,10 +14,10 @@ const Details = ({ track }) => {
     return (
         <DetailsContainer>
             <SongInfoCard>
-                <a><SongImage src={track.track?.album.images[0].url} alt={track.track?.name}/></a>
+                <a href={track.track?.external_urls?.spotify}><SongImage src={track.track?.album.images[0].url} alt={track.track?.name}/></a>
                 <a href={track.track?.external_urls?.spotify}><TrackName className='trackName'>{track.track?.name}</TrackName></a>
-                <a><ArtistName>{track.track?.artists.map((artist) => artist.name).join(", ")}</ArtistName></a>
-                <a><AlbumName>{track.track?.album.name}</AlbumName></a>
+                <a href={track.track?.external_urls?.spotify}><ArtistName>{track.track?.artists.map((artist) => artist.name).join(", ")}</ArtistName></a>
+                <a href={track.track?.external_urls?.spotify}><AlbumName>{track.track?.album.name}</AlbumName></a>
             </SongInfoCard>
             <Memory id={track?.memoryId}></Memory>
         </DetailsContainer>
