9a249a0c020af1e0eadd3403d23ce2ca8cd78807
Jack-Manning11 <jama0209@colorado.edu>
2024-05-01 18:51:28 -0600

    render fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 import Dashboard from './Dashboard';
 import Login from './Login';
-import Landing from './Landing'
 
 import { Container } from './styles/App.styles';
 
 const App = () => {
   const code = new URLSearchParams(window.location.search).get('code');
 
-  return <Landing />;
+  return <Container>{code ? <Dashboard code={code}/> : <Login />}</Container>;
 };
 
 export default App;
-
-//<Container>{code ? <Dashboard code={code}/> : <Login />}</Container>
