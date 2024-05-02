c292103cf6a568659ae8145ddd05f78078c29ff1
Jack-Manning11 <jama0209@colorado.edu>
2024-05-01 18:56:04 -0600

    landing tweak

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import Dashboard from './Dashboard';
 import Login from './Login';
+import Landing from './Landing';
 
 import { Container } from './styles/App.styles';
 
 const App = () => {
   const code = new URLSearchParams(window.location.search).get('code');
 
-  return <Container>{code ? <Dashboard code={code}/> : <Login />}</Container>;
+  return <Landing />;
 };
 
 export default App;
+
+//<Container>{code ? <Dashboard code={code}/> : <Login />}</Container>
