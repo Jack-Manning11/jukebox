28c7efe511c99a38364a32d99915e5c0c7024021
Jack-Manning11 <jama0209@colorado.edu>
2024-05-01 19:25:48 -0600

    help me

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Landing from './Landing';
 import GlobalStyles from "./styles/globalStyles.styles";
 import './styles/font.css';
 
@@ -8,7 +9,7 @@ import './styles/font.css';
 ReactDOM.render(
   <>
     <GlobalStyles />
-    <App />
+    <Landing />
   </>,
   document.getElementById('root')
 );
