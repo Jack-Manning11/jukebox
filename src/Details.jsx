b2e408d28f7d29d29a97b12e973a07db76f35673
Jack-Manning11 <jama0209@colorado.edu>
2024-05-21 15:35:04 -0600

    details fix

--- src/Details.jsx
+++ src/Details.jsx
@@ -6,6 +6,7 @@ import {
     TrackName,
     ArtistName,
     AlbumName,
+    BackButtonLink
 } from './styles/Dashboard.styles';
 import Memory from './Memory';
 
@@ -14,10 +15,10 @@ const Details = ({ track }) => {
     return (
         <DetailsContainer>
             <SongInfoCard>
-                <a href={track.track?.album?.external_urls?.spotify}><SongImage src={track.track?.album.images[0].url} alt={track.track?.name}/></a>
-                <a href={track.track?.external_urls?.spotify}><TrackName className='trackName'>{track.track?.name}</TrackName></a>
-                <a href={track.track?.artists[0]?.external_urls?.spotify}><ArtistName>{track.track?.artists.map((artist) => artist.name).join(", ")}</ArtistName></a>
-                <a href={track.track?.album?.external_urls?.spotify}><AlbumName>{track.track?.album.name}</AlbumName></a>
+                <BackButtonLink href={track.track?.album?.external_urls?.spotify}><SongImage src={track.track?.album.images[0].url} alt={track.track?.name}/></BackButtonLink>
+                <BackButtonLink href={track.track?.external_urls?.spotify}><TrackName className='trackName'>{track.track?.name}</TrackName></BackButtonLink>
+                <BackButtonLink href={track.track?.artists[0]?.external_urls?.spotify}><ArtistName>{track.track?.artists.map((artist) => artist.name).join(", ")}</ArtistName></BackButtonLink>
+                <BackButtonLink href={track.track?.album?.external_urls?.spotify}><AlbumName>{track.track?.album.name}</AlbumName></BackButtonLink>
             </SongInfoCard>
             <Memory id={track?.memoryId}></Memory>
         </DetailsContainer>
