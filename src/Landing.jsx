3e16c374581a34ea254faf94734b8eb11545d4f3
Jack-Manning11 <jama0209@colorado.edu>
2024-06-06 08:54:19 -0600

    what is happening

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -220,6 +220,7 @@ const Landing = () => {
     <div className='body-container'>
       <div class="nav-bar-container">
       <div class="nav-bar">
+        <img src='images/transparent.png' alt='testing '></img>
           <div class="drop-down-flex">
           <p id="drop-down" class="not-shown">☰</p>
           <a class="nav-link" target="blank_" href="HOME LINK"><span class="favicon-bg"><img class="favicon" src="images/transparent.png" alt='favicon for csac'/></span></a>
